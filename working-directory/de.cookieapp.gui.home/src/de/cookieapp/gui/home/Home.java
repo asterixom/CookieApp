74f0568f2663ecc7ee2f65970db58f42d06eefff
der-Chris <der_Chris@gmx.org>
2015-05-11 10:13:56 +0200

    added small welcome text

--- working-directory/de.cookieapp.gui.home/src/de/cookieapp/gui/home/Home.java
+++ working-directory/de.cookieapp.gui.home/src/de/cookieapp/gui/home/Home.java
@@ -1,7 +1,13 @@
 package de.cookieapp.gui.home;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
 
 import de.cookieapp.gui.folderitem.FolderItem;
 
@@ -10,9 +16,28 @@ public class Home implements FolderItem {
 	private Long sessionID;
 
 
+	@Override
 	public Composite getContent(Composite tabFolder) {
-		// TODO Auto-generated method stub
-		return new Composite(tabFolder, SWT.NONE);
+		Composite completeComposite = new Composite(tabFolder, SWT.NONE);
+		completeComposite.setLayout(new GridLayout(1,false));
+		
+		createHeader(completeComposite);
+		//createContent(completeComposite);
+
+		return completeComposite;
+	}
+	
+	/**
+	 * Creates headline and log-off button
+	 * @param completeComposite
+	 */
+	private void createHeader(Composite completeComposite) {
+		Composite header = new Composite(completeComposite, SWT.NONE);
+		header.setLayout(new GridLayout(1, false));
+		Label headline = new Label(header, SWT.NONE);
+		headline.setText("Willkommen bei CookieApp,");
+		Label headline2 = new Label(header, SWT.NONE);
+		headline2.setText("der Webseite zum austauschen von Rezepten");		
 	}
 
 	public String getTabItemName() {
