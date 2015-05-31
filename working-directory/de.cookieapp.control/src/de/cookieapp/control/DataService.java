92053fa4f19d367dd5094207a24c9f3a7d5acfcd
der-Chris <der_Chris@gmx.org>
2015-05-31 21:46:00 +0200

    save recipe, login and register work propperly now

--- working-directory/de.cookieapp.control/src/de/cookieapp/control/DataService.java
+++ working-directory/de.cookieapp.control/src/de/cookieapp/control/DataService.java
@@ -29,9 +29,9 @@ public class DataService {
 		} catch (NoSuchAlgorithmException e) {
 			e.printStackTrace();
 		}
-		*/
+		 */
 	}
-	
+
 	public void setRepository(DataProvider dataProvider) {
 		this.dataProvider = dataProvider;
 	}
@@ -68,32 +68,34 @@ public class DataService {
 	}
 
 	public Recipe getRecipe(Long recipeId) {
-		//TODO implement this
-		return null;
+		return dataProvider.getRecipe(recipeId);
 	}
-	
+
 	public ArrayList<Recipe> getRecipesWithName(String name) {
 		return dataProvider.compareToRecipeName(name);
 	}
 
 	public boolean saveRecipe(String recipeName, String recipeDescription,	User user, ArrayList<String> ingredientNames,
 			ArrayList<String> ingredientUnits, ArrayList<String> ingredientQuantity) {
-		boolean flag = true;
+		boolean flag = false;
 		Recipe recipe = RecipeImpl.createRecipe(recipeName, recipeDescription, user, null, null);
-		dataProvider.saveRecipe(recipe, user);
-		Long recipeID = dataProvider.getRecipeID(recipeName);
-		for (int i = 0; i < ingredientNames.size(); i = i + 1) {
-			double quantity = 0.0;
-			String unit = ingredientUnits.get(i);
-			String name = ingredientNames.get(i);
-			try {
-				quantity = Double.valueOf(ingredientQuantity.get(i));
-			} catch (NumberFormatException e) {
-				System.err.println("NumberFormat Exeption. Could not Format the Number for " + name);
-				e.printStackTrace();
+		boolean saved = dataProvider.saveRecipe(recipe, user);
+		if (saved) {
+			// otherwise the ingredients will ne added again, if the recipe is already in database
+			Long recipeID = dataProvider.getRecipeID(recipeName);
+			for (int i = 0; i < ingredientNames.size(); i = i + 1) {
+				double quantity = 0.0;
+				String unit = ingredientUnits.get(i);
+				String name = ingredientNames.get(i);
+				try {
+					quantity = Double.valueOf(ingredientQuantity.get(i));
+				} catch (NumberFormatException e) {
+					System.err.println("NumberFormat Exeption. Could not Format the Number for " + name);
+					e.printStackTrace();
+				}
+				Ingredient ingredient = IngredientImpl.createIngredient(quantity, unit, name);
+				flag = dataProvider.saveIngredient(ingredient, recipeID);
 			}
-			Ingredient ingredient = IngredientImpl.createIngredient(quantity, unit, name);
-			dataProvider.saveIngredient(ingredient, recipeID);
 		}
 		return flag;
 	}
