9a087a56d6939cb5c7098233b33288440788d7b5
chris <der_Chris@gmx.org>
2015-06-15 08:58:54 +0200

    added option to click the recipes in profile

--- working-directory/de.cookieapp.gui.profile/src/de/cookieapp/gui/profile/Profile.java
+++ working-directory/de.cookieapp.gui.profile/src/de/cookieapp/gui/profile/Profile.java
@@ -7,6 +7,9 @@ import java.util.Set;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CTabFolder;
+import org.eclipse.swt.custom.CTabItem;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Font;
@@ -20,10 +23,12 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Widget;
 
+import de.cookieapp.control.ControlService;
 import de.cookieapp.data.model.Comment;
 import de.cookieapp.data.model.Recipe;
 import de.cookieapp.data.model.User;
 import de.cookieapp.gui.folderitem.FolderItem;
+import de.cookieapp.gui.recipe.RecipeTabImpl;
 import de.cookieapp.util.PictureLoader;
 
 public class Profile implements FolderItem {
@@ -38,9 +43,12 @@ public class Profile implements FolderItem {
 	private Composite completeComposite;
 	private Composite favoriteArea;
 	private Composite ownRecipeArea;
+	private ControlService controlService;
+	private CTabFolder tabFolder;
 
 	@Override
 	public Composite getContent(CTabFolder tabFolder) {
+		this.tabFolder = tabFolder;
 		completeComposite = new Composite(tabFolder, SWT.NONE);
 		completeComposite.setLayout(new GridLayout(1,false));
 		if (user != null) {
@@ -122,10 +130,25 @@ public class Profile implements FolderItem {
 				GridLayout gridLayout = new GridLayout(2, true);
 				comentComposite.setLayout(gridLayout);
 				
-				Recipe recipe = recipes.next();
+				final Recipe recipe = recipes.next();
 				
 				recipeLabel = new Label(comentComposite, SWT.NONE);
 				recipeLabel.setText(recipe.getName());
+				recipeLabel.addMouseListener(new MouseListener() {
+					private static final long serialVersionUID = -5558483000466339886L;
+					@Override
+					public void mouseUp(MouseEvent e) {
+						showRecipe(recipe, false);
+					}
+
+					@Override
+					public void mouseDown(MouseEvent e) {
+					}
+
+					@Override
+					public void mouseDoubleClick(MouseEvent e) {
+					}
+				});
 				GridData griddata = new GridData(SWT.LEFT, SWT.CENTER, true, true, 1, 1);
 				recipeLabel.setLayoutData(griddata);
 			
@@ -155,16 +178,63 @@ public class Profile implements FolderItem {
 				GridLayout gridLayout = new GridLayout(2, true);
 				comentComposite.setLayout(gridLayout);
 				
-				Recipe recipe = recipes.next();
+				final Recipe recipe = recipes.next();
 				
 				recipeLabel = new Label(comentComposite, SWT.NONE);
 				recipeLabel.setText(recipe.getName());
+				recipeLabel.addMouseListener(new MouseListener() {
+					private static final long serialVersionUID = -5558483000466339886L;
+					@Override
+					public void mouseUp(MouseEvent e) {
+						showRecipe(recipe, false);
+					}
+
+					@Override
+					public void mouseDown(MouseEvent e) {
+					}
+
+					@Override
+					public void mouseDoubleClick(MouseEvent e) {
+					}
+				});
+				
 				GridData griddata = new GridData(SWT.LEFT, SWT.CENTER, true, true, 1, 1);
 				recipeLabel.setLayoutData(griddata);
 			}
 		} else {
 			Label recipeLabel = new Label(favoriteArea, SWT.NONE);
 			recipeLabel.setText("Noch keine Favoriten Ausgewählt!");
+		}
+	}
+	
+	/**
+	 * Adds the Recipe as Tab to the TabFolder. 
+	 * If select is true, the Tab will be opened automatically. 
+	 * otherwise it will just be added to the Tab Folder
+	 * @param recipe the Recipe to Show
+	 * @param select a flag to show or not show the Tab
+	 */
+	private void showRecipe(Recipe recipe, boolean select) {
+		CTabItem[] ctabs = tabFolder.getItems();
+		boolean flag = false;
+		for (CTabItem cTabItem : ctabs) {
+			if (cTabItem.getText().equals(recipe.getName())) {
+				flag = true;
+			}
+		}
+		if (!flag)  {
+			RecipeTabImpl recipeTab = new RecipeTabImpl();
+			recipeTab.setControlService(controlService);
+			if (user != null) {
+				recipeTab.setUser(user);
+			}
+			Composite recipeComp = recipeTab.getContent(tabFolder, recipe);
+			CTabItem recipeTabItem = new CTabItem(tabFolder, SWT.CLOSE);
+			recipeTabItem.setText(recipe.getName());
+			recipeTabItem.setControl(recipeComp);
+			if (select) {
+				tabFolder.setSelection(recipeTabItem);
+			}
 		}
 	}
 
@@ -193,5 +263,16 @@ public class Profile implements FolderItem {
 		this.user = user;
 	}
 
+	public void setControlService(ControlService controlService) {
+		if (controlService != null) {
+			this.controlService = controlService;
+		}
+	}
+
+	public void unsetControlService(ControlService controlService) {
+		if (this.controlService.equals(controlService)) {
+			this.controlService = null;
+		}
+	}
 
 }
